36167709b361f50db0597531974861cfaf9f5ac5
kralouf <114093052+kralouf@users.noreply.github.com>
2023-11-06 23:10:13 -0600

    stop reading this, you have a lab to do!

---
+++ Setup_WinRM_for_Ansible.ps1
@@ -0,0 +1,14 @@
+﻿Enable-PSRemoting -Force
+Set-Service WinRM -StartupType 'Automatic'
+Set-Item -Path WSMan:\localhost\Service\Auth\Certificate -Value $true
+Set-Item -Path 'WSMan:\localhost\Service\AllowUnencrypted' -Value $true
+Set-Item -Path 'WSMan:\localhost\Service\Auth\Basic' -Value $true
+Set-Item -Path 'WSMan:\localhost\Service\Auth\CredSSP' -Value $true
+$cert = New-SelfSignedCertificate -DnsName "10.222.104.4" -CertStoreLocation "cert:\LocalMachine\My"
+winrm create winrm/config/Listener?Address=*+Transport=HTTPS "@{Hostname=`"10.222.104.4`";CertificateThumbprint=`"$($cert.Thumbprint)`"}"
+New-NetFirewallRule -DisplayName "Allow WinRM HTTPS" -Direction Inbound -LocalPort 5986 -Protocol TCP -Action Allow
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value "*" -Force
+New-ItemProperty -Name LocalAccountTokenFilterPolicy -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -PropertyType DWord -Value 1 -Force
+Set-ExecutionPolicy Unrestricted -Force
+Restart-Service WinRM
+winrm enumerate winrm/config/Listener
